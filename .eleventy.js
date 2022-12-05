1b6c92722f57704a6b2e655cdcca92099059af3e
Jed Fox <git@jedfox.com>
2022-12-05 17:49:28 -0500

    11ty

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const fs = require('node:fs');
+const cheerio = require('cheerio');
+
+/** @type {(config: import('@11ty/eleventy/src/UserConfig')) => void} */
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("fonts");
+    eleventyConfig.addPassthroughCopy("maps/roads.svg");
+    eleventyConfig.addPassthroughCopy("*.css");
+    eleventyConfig.addPassthroughCopy("jquery-3.6.1.min.js");
+    eleventyConfig.addShortcode('include_map', (name) => {
+        const source = fs.readFileSync(`./maps/${name}.svg`, 'utf8');
+        const $ = cheerio.load(source);
+        return $('svg').children().toArray().map(c => $(c).html())
+    })
+}
